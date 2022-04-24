6241350b164753a66e5672e51e74580a3e5bb7b1
Paramesh Sidhanatham <p.sidhanatham@zensar.com>
2022-04-24 18:25:34 +0530

    Adding

---
+++ Java_Training/Trainer_Manish/Day_2/Array/Example1.java
@@ -0,0 +1,7 @@
+package Java_Training.Trainer_Manish.Day_2.Array;
+
+public class Example1 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Java_Training/Trainer_Manish/Day_2/Beans/Example1.java
@@ -0,0 +1,5 @@
+package Java_Training.Trainer_Manish.Day_2.Beans;
+
+public class Example1 {
+    
+}
