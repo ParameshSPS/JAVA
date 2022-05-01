1db30671769a7e6ab60420e6c5729d8168fd6bd2
Paramesh Sidhanatham <p.sidhanatham@zensar.com>
2022-05-01 09:25:02 +0530

    Adding New Files

---
+++ Core_Java_Course/Non_Primitive_DataTypes.java
@@ -0,0 +1,23 @@
+package Core_Java_Course;
+
+public class Non_Primitive_DataTypes {
+    public static void main(String[] args) {
+
+        // Use Multiple Types
+        String str = "Test"; 
+        System.out.println("String is: " + str);
+
+        String str1 = new String("Test");
+        System.out.println("Another String is: " + str1);
+
+        int arr[];
+        arr = new int[3];
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+
+        System.out.println(arr);
+        System.out.println(arr[0]);
+
+    }
+}
