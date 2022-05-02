a04b53c56b3695fff1b64d086dac0cbc8c243e1b
Paramesh Sidhanatham <p.sidhanatham@zensar.com>
2022-05-02 21:58:40 +0530

    Update

---
+++ Java_Course/Loops/Enhanced_ForLoop.java
@@ -0,0 +1,7 @@
+package Java_Course.Loops;
+
+public class Enhanced_ForLoop {
+    public static void main(String[] args) {
+        
+    }
+}
--- Java_Course/Loops/WhileLoop.java
+++ Java_Course/Loops/WhileLoop.java
@@ -1,15 +1,13 @@
 package Java_Course.Loops;
 
-public class Dowhile {
+public class WhileLoop {
     public static void main(String[] args) {
         int count = 1;
-        // int count = 11;
-        // int count = 10;
 
-        do {
+        while (count < 10) {
             System.out.println("Count is: " + count);
             count++;
-        } while (count <= 10);
-
+        }
+        
     }
 }
