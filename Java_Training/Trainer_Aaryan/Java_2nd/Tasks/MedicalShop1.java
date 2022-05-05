34bfcee3d121f4edee78931d3376a88672004c14
Paramesh Sidhanatham <p.sidhanatham@zensar.com>
2022-05-05 13:08:15 +0530

    Update

--- Java_Training/Trainer_Aaryan/Java_2nd/Tasks/MedicalShop1.java
+++ Java_Training/Trainer_Aaryan/Java_2nd/Tasks/MedicalShop1.java
@@ -10,13 +10,16 @@ class Details {
     String phone_No;
     int amount;
 
-    public Details(String id, String name, int age, String disease_Name, String phone_No, int amount) {
-        this.id = id;
+    public Details(String id2, String name, int age, String disease_Name, String phoneno, int amount) {
+        this.id = id2;
         this.name = name;
         this.age = age;
         this.disease_Name = disease_Name;
-        this.phone_No = phone_No;
+        this.phone_No = phoneno;
         this.amount = amount;
+    }
+
+    public Details(int i, String name2, int age2, String disease_Name2, int j, int amount2) {
     }
 
     public String getId() {
@@ -66,6 +69,12 @@ class Details {
     public void setAmount(int amount) {
         this.amount = amount;
     }
+
+    @Override
+    public String toString() {
+        return "Details [age=" + age + ", amount=" + amount + ", disease_Name=" + disease_Name + ", id=" + id
+                + ", name=" + name + ", phone_No=" + phone_No + "]";
+    }
 }
 
 public class MedicalShop1 {
@@ -89,23 +98,30 @@ public class MedicalShop1 {
         String diseasename = sc1.nextLine();
 
         System.out.print("Enter your phone no: ");
-        String phoneno = sc2.nextLine();
+        String phoneno = sc2.nextLine() + "";
 
         System.out.print("Enter your doctor fee: ");
         int fee = scanner.nextInt();
 
-        Details data = new Details(id, name, age, diseasename, phoneno, fee);
-
+        Details d = new Details(id, name, age, diseasename, phoneno, fee);
         // stack.push(data);
         // System.out.println(data);
 
         scanner.close();
         sc1.close();
         sc2.close();
-        return data;
+        return d;
     }
 
     public static void main(String[] args) {
+
+        Details p1, p2;
+
+        p1 = new Details(1, "param", 23, "cold", 1234, 34);
+        p2 = new Details(2, "Aaryan", 30, "Cold", 23455, 55);
+
+        System.out.println(p1);
+        System.out.println(p2);
 
         // enterDetails();
 
@@ -115,13 +131,13 @@ public class MedicalShop1 {
 
         // System.out.println(stack);
 
-        stack.forEach(elements -> {
-            System.out.println("ID = " + elements.getId());
-            System.out.println("Name = " + elements.getName());
-            System.out.println("Age = " + elements.getAge());
-            System.out.println("Disease = " + elements.getDisease_Name());
-            System.out.println("Contact = " + elements.getPhone_No());
-            System.out.println("Doctor fee = " + elements.getAmount());
-        });
+        // stack.forEach(elements -> {
+        //     System.out.println("ID = " + elements.getId());
+        //     System.out.println("Name = " + elements.getName());
+        //     System.out.println("Age = " + elements.getAge());
+        //     System.out.println("Disease = " + elements.getDisease_Name());
+        //     System.out.println("Contact = " + elements.getPhone_No());
+        //     System.out.println("Doctor fee = " + elements.getAmount());
+        // });
     }
 }
