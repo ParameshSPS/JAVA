c262f6709b1216a2d0a58364c1a8ddc4f27b0063
Paramesh Sidhanatham <p.sidhanatham@zensar.com>
2022-05-13 10:56:38 +0530

    Update Files

---
+++ Java_Training/Trainer_Aaryan/Java_May_13th/Date_Difference.java
@@ -0,0 +1,25 @@
+package Java_Training.Trainer_Aaryan.Java_May_13th;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+public class Date_Difference {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        // LocalDate birthday = LocalDate.of(2001, Month.JULY, 7);
+        LocalDate birthday = LocalDate.of(1998, Month.APRIL, 4);
+
+        Period p = Period.between(birthday, today);
+        long p2 = ChronoUnit.DAYS.between(birthday, today);
+
+        // Calculate age
+        System.out.println("You are " + p.getYears() + " years");
+        System.out.println(p.getMonths() + " months");
+        System.out.println(p.getDays() + " days old. (" + p2 + " days total)");
+
+        // System.out.println("\nYou are " + p.getYears() + " years, " + p.getMonths() +
+        // " months, and " + p.getDays() + " days old. (" + p2 + " days total)");
+    }
+}
---
+++ Java_Training/Trainer_Aaryan/Java_May_13th/Task.java
@@ -0,0 +1,5 @@
+package Java_Training.Trainer_Aaryan.Java_May_13th;
+
+public class Task {
+    
+}
